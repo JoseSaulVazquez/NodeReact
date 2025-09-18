901fdbf6566314705c533dbe7a4164b15cc500de
JoseSaulVazquez <jose.vazquez.22s@utzmg.edu.mx>
2025-09-18 15:32:58 -0600

    segundo commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 import './index.css'
-import AppRouter from './router/AppRouter.jsx'
+import App from './App.jsx'
+
+navigator.serviceWorker.register('/sw.js');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
