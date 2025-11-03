10c19291c9f78c55fd18099d5d2319f7092b4946
JoseSaulVazquez <jose.vazquez.22s@utzmg.edu.mx>
2025-11-03 13:02:02 -0600

    Doceavo commit

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// ==========================
+
 // CLAVES VAPID
-// ==========================
+
 const publicVapidKey =
   "BIFfnwJktLiHzU4hsToHUkjNoPia0L4XuEcIyt3m3PeTHxo9oCSKdgNSWeIP2RS37p5ulxnP0Twzt86hLt8PQuQ";
 const privateVapidKey = "VYccipkuFENALikvb_Eb0Hs9dxKkEDFQxBpyXDtgq5w";
@@ -21,9 +21,9 @@ webpush.setVapidDetails(
   privateVapidKey
 );
 
-// ==========================
+
 // CONEXIÓN A MONGO
-// ==========================
+
 mongoose
   .connect(
     "mongodb+srv://Saul_ioT:1234@cluster0.fo4lgsw.mongodb.net/pwaDB?retryWrites=true&w=majority&appName=Cluster0",
@@ -32,9 +32,9 @@ mongoose
   .then(() => console.log("Conectado a MongoDB"))
   .catch((err) => console.error("Error al conectar MongoDB:", err));
 
-// ==========================
+
 // MODELOS
-// ==========================
+
 const RegistroSchema = new mongoose.Schema({
   nombre: String,
   fecha: { type: Date, default: Date.now },
@@ -49,9 +49,8 @@ const SubscriptionSchema = new mongoose.Schema({
 const Registro = mongoose.model("Registro", RegistroSchema);
 const Subscription = mongoose.model("Subscription", SubscriptionSchema);
 
-// ==========================
 // ENDPOINT: guardar datos normales
-// ==========================
+
 app.post("/api/save", async (req, res) => {
   const { nombre } = req.body;
   const nuevo = new Registro({ nombre });
@@ -60,9 +59,9 @@ app.post("/api/save", async (req, res) => {
   res.status(201).json({ message: "Guardado correctamente", data: nuevo });
 });
 
-// ==========================
+
 // ENDPOINT: guardar suscripción push
-// ==========================
+
 app.post("/api/subscribe", async (req, res) => {
   const { subscription, luchador } = req.body; // <-- el cliente puede mandar un luchador
   if (!subscription) return res.status(400).json({ message: "Falta suscripción" });
@@ -77,9 +76,9 @@ app.post("/api/subscribe", async (req, res) => {
   res.status(201).json({ message: "Suscripción guardada correctamente." });
 });
 
-// ==========================
+
 // ENDPOINT: enviar push general
-// ==========================
+
 app.post("/api/send-push", async (req, res) => {
   const { title, body } = req.body;
 
@@ -97,11 +96,9 @@ app.post("/api/send-push", async (req, res) => {
   res.json({ message: "Notificaciones enviadas" });
 });
 
-// ==========================
-// ENDPOINT: enviar push personalizado
-// ==========================
 
-// Ejemplo: POST /api/send-push/CM%20Punk
+// ENDPOINT: enviar push personalizado
+
 app.post("/api/send-push/:luchador", async (req, res) => {
   const { luchador } = req.params;
   const { title, body } = req.body;
@@ -126,9 +123,21 @@ app.post("/api/send-push/:luchador", async (req, res) => {
   res.json({ message: `Notificaciones enviadas a fans de ${luchador}` });
 });
 
-// ==========================
+
+// Cancelar suscripción
+app.post("/api/unsubscribe", async (req, res) => {
+  const { endpoint } = req.body;
+  if (!endpoint) return res.status(400).json({ message: "Falta endpoint" });
+
+  await Subscription.findOneAndDelete({ endpoint });
+
+  console.log("Suscripción eliminada:", endpoint);
+
+  res.json({ message: "Suscripción cancelada correctamente" });
+});
+
+
 // SERVIDOR
-// ==========================
 const PORT = 4000;
 app.listen(PORT, () =>
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
